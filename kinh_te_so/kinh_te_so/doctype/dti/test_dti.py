255de404abbb2534daae6359dbc9993e5f4f20a9
rnd-nguyenth <nguyenth@osbholding.com>
2025-02-13 08:51:09 +0700

    13/02/2025

--- kinh_te_so/kinh_te_so/doctype/dti/test_dti.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Nguyenth and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestDTI(FrappeTestCase):
-	pass
--- kinh_te_so/kinh_te_so/doctype/dti_input/dti_input.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Nguyenth and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class DTIInput(Document):
-	pass

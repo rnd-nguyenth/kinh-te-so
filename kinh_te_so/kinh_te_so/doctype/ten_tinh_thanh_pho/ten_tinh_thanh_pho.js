bc4015a8798f354b1bc87998957fe9a39a9193f5
rnd-nguyenth <nguyenth@osbholding.com>
2025-02-26 09:34:49 +0700

    26/02/2025

---
+++ kinh_te_so/kinh_te_so/doctype/ten_tinh_thanh_pho/ten_tinh_thanh_pho.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Nguyenth and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Ten_Tinh_Thanh_Pho", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ kinh_te_so/kinh_te_so/doctype/xep_hang_dti/xep_hang_dti.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Nguyenth and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Xep_Hang_DTI", {
+// 	refresh(frm) {
+
+// 	},
+// });
